e73d376bd7f4396710d181b1d0f1c87bb1584424
Qinming JIANG <jiangqinming005@gmail.com>
2023-02-22 20:42:24 +0100

    complete caching system of the endpoint '/api/v1/product'

--- back-end/src/product/product.controller.ts
+++ back-end/src/product/product.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   CacheInterceptor,
+  CacheKey,
   CACHE_MANAGER,
   Controller,
   Get,
@@ -10,23 +11,16 @@ import {
 } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { ProductService } from "./product.service";
-import Cache from 'cache-manager';
 
 @UseInterceptors(CacheInterceptor)
 @Controller("/api/v1/product")
 export class ProductController {
-  constructor(
-    private readonly productService: ProductService,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache
-  ) {}
+  constructor(private readonly productService: ProductService) {}
 
+  @CacheKey("custom_key")
   @UseGuards(AuthGuard("jwt"))
   @Get(":body")
-  async getProduct(@Body() body) {
-    const product = this.productService.getProductByBarcode(body.code);
-    // await this.cacheManager.set(product.id, product);
-    // const value = await this.cacheManager.get('key');
-
-    return product;
+  getProduct(@Body() body) {
+    return this.productService.getProductByBarcode(body.code);
   }
 }
--- back-end/src/product/product.service.ts
+++ back-end/src/product/product.service.ts
@@ -1,31 +1,41 @@
 import {
+  CACHE_MANAGER,
   ForbiddenException,
+  Inject,
   Injectable,
   NotFoundException,
-} from '@nestjs/common';
-import { HttpService } from '@nestjs/axios';
-import axios from 'axios';
+} from "@nestjs/common";
+import { HttpService } from "@nestjs/axios";
+import axios from "axios";
+import { Cache } from "cache-manager";
 
 @Injectable()
 export class ProductService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   async getProductByBarcode(code: string) {
-    const url = `https://world.openfoodfacts.org/api/v2/product/${code}`;
+    const value = await this.cacheManager.get(code);
 
-    const response = await axios({
-      method: 'GET',
-      url: `${url}`,
-    }).catch(() => {
-      throw new ForbiddenException('API not available');
-    });
+    if (!value) {
+      const response = await axios({
+        method: "GET",
+        url: `https://world.openfoodfacts.org/api/v2/product/${code}`,
+      }).catch(() => {
+        throw new ForbiddenException("API not available");
+      });
+  
+      if (!response.data?.product) {
+        throw new NotFoundException(`Product ${code} doesn't exist.`);
+      }
 
-    if (!response.data?.product) {
-      throw new NotFoundException(`Product ${code} doesn't exist.`);
+      await this.cacheManager.set(code, response.data, 10000);
+
+      return response.data;
     }
 
-    return {
-      product: response.data?.product,
-    };
+    return value;
   }
 }
